18438871c68a19b64904ceff513dde01348cc01f
Mightnire <mightnire@gmail.com>
2024-04-07 17:54:49 +0800

    another submit

--- src/main/jdbc/JdbcTemplateCase.java
+++ src/main/jdbc/JdbcTemplateCase.java
@@ -5,6 +5,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import java.sql.PreparedStatement;
+import java.util.List;
+import java.util.Map;
 
 // JDBC
 public class JdbcTemplateCase {
@@ -34,14 +36,44 @@ public class JdbcTemplateCase {
         jdbcTemplate.update(deleteSQL,"zhangsan");
 
         // lambda
-        jdbcTemplate.update(
-                con -> {
-                    PreparedStatement ps = con.prepareStatement(insertSQL);
-                    ps.setString(1,"lili");
-                    ps.setString(2,"mimi");
-                    return ps;
-                }
-        );
+//        jdbcTemplate.update(
+//                con -> {
+//                    PreparedStatement ps = con.prepareStatement(insertSQL);
+//                    ps.setString(1,"lili");
+//                    ps.setString(2,"mimi");
+//                    return ps;
+//                }
+//        );
+
+        // query
+//
+        String selectsql0 = "select address from wp_user where name = ?";
+        String res1 = jdbcTemplate.queryForObject(selectsql0,String.class,"lili");
+        System.out.println(res1);
+
+        String selectsql1 = "select address from wp_user";
+        List<String> addressList = jdbcTemplate.queryForList(selectsql1,String.class);
+        for (String s : addressList) {
+            System.out.println(s);
+        }
+
+        String selectall = "select * from wp_user";
+        List<Map<String,Object>> result = jdbcTemplate.queryForList(selectall);
+        for (Map<String,Object> map: result){
+            for(String s: map.keySet()){
+                System.out.println(s + "->" + map.get(s));
+            }
+        }
+
+
+        String selectsql2 = "select * from wp_user where name = ?";
+        Person p = jdbcTemplate.queryForObject(selectsql2,(rs,rsNum) -> {
+            Person person = new Person();
+            person.setAddress(rs.getString("address"));
+            person.setName(rs.getString("name"));
+            return person;
+        },"lili");
+        System.out.println(p.toString());
     }
 
 
